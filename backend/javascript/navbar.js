58e16a6df5a3c57f4fe624a1db43890e26119f5a
Szbalazs28 <kvart276@gmail.com>
2025-11-14 20:16:26 +0100

    navigacios_sav_mukodese

--- backend/javascript/navbar.js
+++ backend/javascript/navbar.js
@@ -1,8 +1,14 @@
-
-const mainpage_display = document.getElementById("fooldal")
-const account_display = document.getElementById("felhasznalo")
-
-function navbar_click(a){
-   
+function navbar_click(id, sorszam) {
+    document.querySelectorAll(".mainelem").forEach(element => {
+        if (!element.classList.contains("dnone")) {
+            element.classList.add("dnone")            
+        }
+    })
+    document.querySelectorAll(".nav_items").forEach(element => {
+        if(element.classList.contains("active")){
+            element.classList.remove("active")
+        }
+    })
+    document.querySelector(`.nav_items_div > div:nth-child(${sorszam})`).classList.add("active")
+    document.getElementById(id).classList.remove("dnone")
 }
-
