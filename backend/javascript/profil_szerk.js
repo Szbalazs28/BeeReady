4d79ecf075d53df2a51aa500f927e935e23967fd
SzeHNR <120087317+SzeHNR@users.noreply.github.com>
2025-11-15 22:08:09 +0100

    profilvaltas js

--- backend/javascript/profil_szerk.js
+++ backend/javascript/profil_szerk.js
@@ -4,3 +4,28 @@ async function felhasznalo_betoltes(){
     document.getElementById("szerkusername").value = adat.username
     document.getElementById("szerkemail").value = adat.email
 }
+
+
+
+//kep valtoztatasa (a modal/overlay fullosan mukodik)
+const profilkep = document.getElementById("prof_img");
+const modal = document.getElementById("profil_modal");
+const close = document.getElementById("close");
+const kepek = document.querySelectorAll(".avatar_option");
+const src = profilkep.querySelector("img");
+
+profilkep.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+kepek.forEach(karakter => {
+    karakter.addEventListener("click", () => {
+        const uj = karakter.src;
+        src.src = uj; 
+        modal.style.display = "none"; 
+    });
+});
