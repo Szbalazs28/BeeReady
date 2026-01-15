f4fe1586c18c1332b7ee30ed1c627cc6459ed4dd
SzeHNR <120087317+SzeHNR@users.noreply.github.com>
2026-01-15 19:17:59 +0100

    `Added Pomodoro timer functionality to main.html and created timer.js script`

---
+++ frontend/javascript/timer.js
@@ -0,0 +1,48 @@
+let timer;
+let isRunning = false;
+let timeLeft = 25 * 60;
+const timerDisplay = document.getElementById('timer_display');
+const startB = document.getElementById('start_timer');
+const pauseB = document.getElementById('stop_timer');
+const resetB = document.getElementById('reset_timer');
+
+function updateDisplay() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    timerDisplay.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+// = padStart(`mennyi a kivant szamhossz`, `mi legyen a kiegeszitokarakter`)
+
+function start() {
+    if (!isRunning) {
+        isRunning = true;
+        timer = setInterval(() => {
+            if (timeLeft > 0) {
+                timeLeft--;
+                updateDisplay();
+            }
+            else {
+                clearInterval(timer);
+                isRunning = false;
+                timerDisplay.textContent = 'Idő lejárt! Ideje egy kis szünetet tartani.';
+            }
+        }, 1000);
+    }
+    else { return }
+}
+function pause() {
+    if (!isRunning) {
+        clearInterval(timer);
+        isRunning = false;
+    }
+    else { return };
+
+}
+function reset() {
+    clearInterval(timer);
+    isRunning = false;
+    timeLeft = 25 * 60;
+    updateDisplay();
+}
+
+updateDisplay();
