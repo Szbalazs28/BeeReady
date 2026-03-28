25e6233c13a027add1b8a8021199361eafce71e5
SzeHNR <120087317+SzeHNR@users.noreply.github.com>
2026-03-28 13:02:32 +0100

    `Updated frontend CSS and JavaScript files for Hive feature`

---
+++ frontend/javascript/hive.js
@@ -0,0 +1,6 @@
+const heartBtn = document.getElementById('hive_btn_save')
+
+heartBtn.addEventListener('click', () => {
+    const isSaved = heartBtn.classList.toggle('saved');
+    heartBtn.textContent = isSaved ? '♥' : '♡';
+});
